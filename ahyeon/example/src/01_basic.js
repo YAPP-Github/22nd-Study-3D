6c5a6593a9768412b0091b5319dfffda0bd81ab8
naro_Kim <gracias9022@gmail.com>
2023-05-24 15:56:06 +0900

    chore: 01강 기본 파일 추가 및 브랜치 생성

---
+++ ahyeon/example/src/01_basic.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+import "./01_basic.css";
+
+class App {
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+		this._divContainer = divContainer;
+
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
+		renderer.setPixelRatio(window.devicePixelRatio);
+		divContainer.appendChild(renderer.domElement);
+		this._renderer = renderer;
+
+		const scene = new THREE.Scene();
+		this._scene = scene;
+
+		this._setupCamera();
+		this._setupLight();
+		this._setupModel();
+
+		window.onresize = this.resize.bind(this);
+		this.resize();
+
+		requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+window.onload() = () => {
+  new App;
+}
