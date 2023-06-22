4055d1a8be9f0574096d376684a910f54ab16cff
Sangjun-man <molung@icloud.com>
2023-06-22 21:20:44 +0900

    feat : #5-4 텍스쳐 매핑 실습

--- sangjun/example/src/main.ts
+++ sangjun/example/src/main.ts
@@ -1,8 +1,8 @@
 // import week1 from "./week1/app";
-import week3 from "./week3/app";
+import week4 from "./week4/app";
 
 import "./style.css";
 
 window.onload = () => {
-  new week3();
+  new week4();
 };
