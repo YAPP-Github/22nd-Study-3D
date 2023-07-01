27cef562e18e1aebf20e64ad134b4b330a850992
HaYeong <hy78733@gmail.com>
2023-07-01 13:12:51 +0900

    feat: shadow.camera의 절두체 크기 늘리기

--- hayeong/example/src/08-shadow.js
+++ hayeong/example/src/08-shadow.js
@@ -139,6 +139,12 @@ class App {
     light.target.position.set(0, 0, 0);
     this._scene.add(light.target);
 
+    light.shadow.camera.top = light.shadow.camera.right = 6;
+    light.shadow.camera.bottom = light.shadow.camera.left = -6;
+
+    const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
+    this._scene.add(cameraHelper);
+
     this._scene.add(light);
     this._light = light;
     light.castShadow = true; // 그림자 효과를 위한 설정
